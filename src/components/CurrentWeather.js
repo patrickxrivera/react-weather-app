5578e9681c40c6ebd39db2879dcb58637ba84dd7
Patrick <patrick.x.rivera@gmail.com>
2018-02-14 17:20:31 -0500

    Created CurrentWeather component

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Sun } from 'react-feather';
 
 function CurrentWeather() {
   return (
-    <div className='CurrentWeather__container'>
-      <div className='CurrentWeather__top'>
-        <div>
-
+    <div className='CurrentWeather__container-main'>
+      <div className='CurrentWeather__container-content'>
+        <div className='CurrentWeather__top'>
+          <div>
+            <Sun size={100}/>
+          </div>
+          <span className='CurrentWeather__temp-main'>65&deg;</span>
         </div>
-      </div>
-      <div className='CurrentWeather__middle'>
-        High: 76 / Low: 45
-      </div>
-      <div className='CurrentWeather__bottom'>
-
+        <div className='CurrentWeather__description'>
+          Windy, chance of a few late night showers. Lows overnight in the mid 50s.
+        </div>
+        <div className='CurrentWeather__bottom'>
+          High: 76&deg; / Low: 45&deg;
+        </div>
       </div>
     </div>
   )
